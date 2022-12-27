892c8652898313b10f91b927b25b6a63b88f19f8
Kihura <76404328+Kihura@users.noreply.github.com>
2022-12-27 11:38:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+const mycanvas = document.getElementsById('enchart');
+new Chart(mycanvas, {
+    type: 'bar',
+    data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+    }]
+    },
+    options: {
+    scales: {
+        y: {
+        beginAtZero: true
+        }
+    }
+    }
+});}
